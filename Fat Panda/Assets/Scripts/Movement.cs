2951ee3418e4d17a7cdbed1f0f9b67a9486fa483
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-03-28 13:35:49 -0700

    Initial commit

---
+++ Fat Panda/Assets/Scripts/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private int movementDirX;
+    private int movementDirY;
+
+    private float pandaSpeed = 4.5f;
+    private Vector2 movement;
+
+    private Rigidbody2D rig;
+
+    private GameObject objectHeld;
+
+    private VariableJoystick movementJoystick;
+    public VariableJoystick aimingJoystick;
+
+    private Vector2 aim;
+    private bool readyToShoot = false;
+
+    void Awake()
+    {
+        rig = transform.GetComponent<Rigidbody2D>();
+        objectHeld = null;
+    }
+
+    void Update()
+    {
+        //Get x and y magnitudes based off keys pressed
+        movementDirX = 0;
+        if (Input.GetKey(KeyCode.D))
+            movementDirX++;
+        if (Input.GetKey(KeyCode.A))
+            movementDirX--;
+
+        movementDirY = 0;
+        if (Input.GetKey(KeyCode.W))
+            movementDirY++;
+        if (Input.GetKey(KeyCode.S))
+            movementDirY--;
+
+        //panda follows movement vector at a certain speed
+        rig.velocity = pandaSpeed * new Vector2(movementDirX, movementDirY);
+
+        PrepareToThrowAnObject();
+    }
+
+    private void PrepareToThrowAnObject()
+    {
+        //Keep track of aim when Player is using joystick to aim
+        if (objectHeld && aimingJoystick.Direction != default(Vector2))
+        {
+            aim = aimingJoystick.Direction;
+            readyToShoot = true;
+        }
+
+        //Throw object when joystick is released
+        if (readyToShoot && aimingJoystick.Direction == default(Vector2))
+        {
+            ThrowObject(aim);
+            readyToShoot = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Panda holds an object it picks up
+        if (collision.gameObject.layer == 8)
+        {
+            //object becomes child of Panda gameObject and teleports to Panda
+            objectHeld = collision.gameObject;
+            objectHeld.transform.parent = transform;
+            objectHeld.transform.position = transform.GetChild(0).transform.position;
+        }
+    }
+
+    private void ThrowObject(Vector2 dir)
+    {
+        
+        //detach object from Panda
+        objectHeld.transform.parent = null;
+        objectHeld.layer = 10;
+
+        //launch object and apply curve effect through gravity
+        objectHeld.AddComponent<Rigidbody2D>();
+        objectHeld.transform.GetComponent<Rigidbody2D>().gravityScale = Mathf.Sign(dir.y);
+        objectHeld.transform.GetComponent<Rigidbody2D>().AddForce(dir * 400);
+
+        //Object must stop eventually, as if it landed on the ground
+        StartCoroutine(objectLandsOnFloorEventually(objectHeld, gameObject.transform.position.y, Mathf.Sign(dir.y)));
+
+        //Panda no longer is holding an object
+        objectHeld = null;
+    }
+
+    private IEnumerator objectLandsOnFloorEventually(GameObject thrownObject, float groundLevel, float curveDirection)
+    {
+        Debug.Log(groundLevel + ", " + curveDirection);
+        //object is in the air for max 22*0.05 = 1.1 seconds
+        for (int i = 1; i <= 22; i++)
+        {
+            yield return new WaitForSeconds(0.05f);
+
+            if (thrownObject.transform.position.y < groundLevel - 1f && curveDirection == 1f)
+                break;
+            else if (thrownObject.transform.position.y > groundLevel + 1f && curveDirection == -1f)
+                break;
+        }
+
+        //Stop object's movement 
+        Destroy(thrownObject.transform.GetComponent<Rigidbody2D>());
+
+        //Make the object repickable for testing purposes
+        thrownObject.layer = 8;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Covert Panda
+ Game where you're a panda breaking into Area 51. What could go wrong?
