23104e9efd088633a44a49a11afa7863a33650ef
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-04-15 14:19:13 -0700

    testing

---
+++ Fat Panda 2/Assets/Scripts/AI/EnvironmentDetection.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentDetection 
+{
+    private LayerMask map = (1 << 11);
+
+    private Dictionary<EnvKey, EnvInfo> info = new Dictionary<EnvKey, EnvInfo>();
+
+    //cast multiple raycasts to gather info about the environment
+    public void scanEnvironment(Transform entity) {
+
+        info.Clear();
+
+        float eX = entity.transform.position.x;
+        float eY = entity.transform.position.y;
+
+
+        //A|B represents: type of check | position check starts
+        //R = right, L = left, then U = Upper, D = Lower
+ 
+        //G for ground check
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 0, 0), Color.magenta, new Vector2(eX, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 1, 0), Color.magenta, new Vector2(eX + 3, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 2, 0), Color.magenta, new Vector2(eX + 6, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 3, 0), Color.magenta, new Vector2(eX + 9, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 4, 0), Color.magenta, new Vector2(eX + 12, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', 5, 0), Color.magenta, new Vector2(eX + 15, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', -1, 0), Color.magenta, new Vector2(eX - 3, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', -2, 0), Color.magenta, new Vector2(eX - 6, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', -3, 0), Color.magenta, new Vector2(eX - 9, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', -4, 0), Color.magenta, new Vector2(eX - 12, eY));
+        sendRaycast(Vector2.down, 6, new EnvKey('G', -5, 0), Color.magenta, new Vector2(eX - 15, eY));
+
+        //L for left wall check
+        sendRaycast(Vector2.left, 14, new EnvKey('L', 0, 0), Color.blue, new Vector2(eX, eY));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', 0, 1), Color.blue, new Vector2(eX, eY + 2.5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', 0, -1), Color.blue, new Vector2(eX, eY - 2.5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', 0, 2), Color.blue, new Vector2(eX, eY + 5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', 0, -2), Color.blue, new Vector2(eX, eY - 5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', -1, 1), Color.blue, new Vector2(eX - 2.5f, eY + 2.5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', -1, -1), Color.blue, new Vector2(eX - 2.5f, eY - 2.5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', -1, 2), Color.blue, new Vector2(eX-2.5f, eY + 5f));
+        sendRaycast(Vector2.left, 14, new EnvKey('L', -1, -2), Color.blue, new Vector2(eX-2.5f, eY - 5f));
+
+        //R for right wall check
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 0, 0), Color.green, new Vector2(eX, eY));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 0, 1), Color.green, new Vector2(eX, eY + 2.5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 0, -1), Color.green, new Vector2(eX, eY - 2.5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 0, 2), Color.green, new Vector2(eX, eY + 5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 0, -2), Color.green, new Vector2(eX, eY - 5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 1, 1), Color.green, new Vector2(eX + 2.5f, eY + 2.5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 1, -1), Color.green, new Vector2(eX + 2.5f, eY - 2.5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 1, 2), Color.green, new Vector2(eX + 2.5f, eY + 5f));
+        sendRaycast(Vector2.right, 14, new EnvKey('R', 1, -2), Color.green, new Vector2(eX + 2.5f, eY - 5f));
+
+        //C for ceiling check
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 0, 0), Color.cyan, new Vector2(eX, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 1, 0), Color.cyan, new Vector2(eX + 3, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 2, 0), Color.cyan, new Vector2(eX + 6, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 3, 0), Color.cyan, new Vector2(eX + 9, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 4, 0), Color.cyan, new Vector2(eX + 12, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', 5, 0), Color.cyan, new Vector2(eX + 15, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', -1, 0), Color.cyan, new Vector2(eX - 3, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', -2, 0), Color.cyan, new Vector2(eX - 6, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', -3, 0), Color.cyan, new Vector2(eX - 9, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', -4, 0), Color.cyan, new Vector2(eX - 12, eY));
+        sendRaycast(Vector2.up, 6, new EnvKey('C', -5, 0), Color.cyan, new Vector2(eX - 15, eY));
+
+        /* foreach (KeyValuePair<string, EnvInfo> kvp in info)
+            Debug.LogFormat("{0}: {1}, {2}", kvp.Key, kvp.Value.getHit(), kvp.Value.getHitPoint()) ;
+        */
+
+    }
+
+    //request the info about the environment
+    public Dictionary<EnvKey, EnvInfo> requestEnvironmentInfo()
+    {
+        return info;
+    }
+
+    private void sendRaycast(Vector2 dir, float distance, EnvKey nameOfCheck, Color color, Vector2 entityPos)
+    {
+        //send Raycast, check if it hit a collider, and draw ray visually too
+        RaycastHit2D hit = Physics2D.Raycast(entityPos, dir, distance, map);
+        Vector2 hitPos = (hit.collider != null) ? hit.point : entityPos + dir * distance;
+        Debug.DrawLine(entityPos, hitPos, color, 4f);
+
+        //return info
+        info.Add(nameOfCheck, new EnvInfo(hitPos, hit.collider != null));
+    }
+}
---
+++ Fat Panda 2/Assets/Scripts/Panda/Sideview_Controller.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sideview_Controller : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private SpriteRenderer sR;
+    private Animator animator;
+
+    private GameObject objectHeld;
+    private Camera camera;
+
+    public float speed = 2.0f;
+    public float jumpForce = 200;
+
+    public float throwForce = 500;
+    public Vector2 objectSpinSpeed = new Vector2(-200, 200);
+
+    private int movementDirX;
+
+    void Awake()
+    {
+        rig = transform.GetComponent<Rigidbody2D>();
+        animator = transform.GetChild(0).transform.GetComponent<Animator>();
+        sR = transform.GetChild(0).transform.GetComponent<SpriteRenderer>();
+        camera = transform.GetChild(1).transform.GetComponent<Camera>();
+
+        objectHeld = null;
+    }
+
+    void Update()
+    {
+        //convert A and D keys to left or right movement
+        movementDirX = 0;
+        if (Input.GetKey(KeyCode.D))
+            movementDirX++;
+        if (Input.GetKey(KeyCode.A))
+            movementDirX--;
+
+        rig.velocity = new Vector2(speed * movementDirX, rig.velocity.y);
+
+        //convert W and S keys to jumping up or thrusting downwards
+        if (Input.GetKeyDown(KeyCode.W))
+            rig.AddForce(new Vector2(0, jumpForce));
+        if (Input.GetKeyDown(KeyCode.S))
+            rig.AddForce(new Vector2(0, -jumpForce));
+
+        //Right click to throw
+        if (Input.GetMouseButtonDown(0) && objectHeld)
+            throwObject();
+
+        //orient the player to face the same direction they're moving in
+        playerOrientation();
+
+        //set idle or move animations
+        playerAnimation();
+    }
+
+    private void playerOrientation()
+    {
+        if (rig.velocity.x > 0) 
+            sR.flipX = true;
+        else if (rig.velocity.x < 0)
+            sR.flipX = false;
+    }
+
+    private void playerAnimation()
+    {
+        if (Mathf.Abs(rig.velocity.x) > 0)
+            animator.SetInteger("State", 1);
+        else
+            animator.SetInteger("State", 0);
+    }
+
+    private void throwObject()
+    {
+        //calculate direction to throw object
+        Vector2 throwDir = (Input.mousePosition - camera.WorldToScreenPoint(transform.position)).normalized;
+
+        //detach object from Player
+        objectHeld.transform.parent = null;
+        objectHeld.layer = LayerMask.NameToLayer("Thrown Object");
+
+        //launch object and apply gravity
+        objectHeld.AddComponent<Rigidbody2D>();
+        objectHeld.transform.GetComponent<Rigidbody2D>().gravityScale = 1;
+        objectHeld.transform.GetComponent<Rigidbody2D>().AddForce(throwForce * throwDir);
+        objectHeld.transform.GetComponent<Rigidbody2D>().angularVelocity = Random.Range(objectSpinSpeed.x, objectSpinSpeed.y);
+
+        //Player no longer is holding that object
+        objectHeld = null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Player collides with object it can pick up
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Object") && !objectHeld)
+        {
+            //object becomes child of Player gameObject and teleports to Player
+            objectHeld = collision.gameObject;
+            objectHeld.transform.parent = transform;
+            objectHeld.transform.position = transform.GetChild(0).transform.position;
+        }
+    }
+}
