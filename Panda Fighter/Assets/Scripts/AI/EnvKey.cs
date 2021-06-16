1b486eeaa8b373a3ee568758b0a33f88482f429d
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-06-16 02:23:00 -0700

    Bot AI almost ready

---
+++ Panda Fighter/Assets/Scripts/AI/EnvKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct EnvKey  
+{
+    public int x;
+    public int y;
+    public char direction;
+
+    public EnvKey(char direction, int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
+
+public struct WallChecker
+{
+    public bool wallNearby;
+    public string floorOpening;
+    public string ceilingOpening;
+
+    public WallChecker(bool wallNearby, string floorOpening, string ceilingOpening)
+    {
+        this.wallNearby = wallNearby;
+        this.floorOpening = floorOpening;
+        this.ceilingOpening = ceilingOpening;
+    }
+}
---
+++ Panda Fighter/Assets/Scripts/Player/HP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HP : MonoBehaviour
+{
+    public static int playerHP = 100;
+    private static int maxPlayerHP = 100;
+
+    public Image hpBar;
+
+    void Awake()
+    {
+        playerHP = maxPlayerHP;
+    }
+
+    void Update()
+    {
+        //update health bar visually 
+        hpBar.fillAmount = (float)playerHP / (float)maxPlayerHP;
+
+        if (playerHP < 0)
+            deathAnimation();
+    }
+
+    private void deathAnimation()
+    {
+        //death animation 
+        SceneManager.LoadScene(1);
+    }
+}
