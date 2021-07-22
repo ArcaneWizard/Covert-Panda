25a40bab406528958fd9acb1389fc654487be575
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-07-22 15:59:29 -0700

    throwing anims implemented in game

--- Panda Fighter/Assets/Scripts/Player/Shooting.cs
+++ Panda Fighter/Assets/Scripts/Player/Shooting.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
 
+    [Header("Entities")]
     public Camera camera;
     public Transform shootingArm;
+    private Animator armAnimator;
 
     private WeaponSystem weaponSystem;
     private Sideview_Controller sideview_Controller;
     private Rigidbody2D rig;
     private GameObject weapon;
-    private float timeLeftBtwnShots;
 
-    public Transform bulletSpawnPoint;
+    private float timeLeftBtwnShots;
+    private Rigidbody2D objectRig;
+    private Vector2 aimDir;
+
+
+    [Header("Shooting + Throwing")]
+    public Transform gunSpawnPoint;
+    public Transform handSpawnPoint;
+    public GameObject shoulderBones_GUN;
+    public GameObject shoulderSprites_GUN;
+    public GameObject frontShoulderBone_HAND;
+    public GameObject backShoulderBone_HAND;
 
     private float grenadeThrowForce = 1800;
     private float grenadeYForce = -20;
@@ -27,26 +39,36 @@ public class Shooting : MonoBehaviour
     {
         weaponSystem = transform.GetComponent<WeaponSystem>();
         sideview_Controller = transform.GetComponent<Sideview_Controller>();
+
         rig = transform.GetComponent<Rigidbody2D>();
+        armAnimator = transform.GetComponent<Animator>();
     }
 
     void Update()
     {
-        //Weapons where you right click to use/shoot the weapon once
+        //after throwing, go back to normal swinging hands walking state
+        if (armAnimator.GetInteger("Arms Phase") == 1)
+            armAnimator.SetInteger("Arms Phase", 0);
+
+        //Weapons where you right click to throw/shoot 
         if (Input.GetMouseButtonDown(0) && weaponSystem.weaponSelected != null)
         {
             if (weaponSystem.getAmmo() > 0 && weaponSystem.getWeapon().tag == "singleFire")
             {
                 weapon = weaponSystem.getWeapon();
-                weaponSystem.usedOneAmmo();
+                weaponSystem.useOneAmmo();
+                objectRig = weapon.transform.GetComponent<Rigidbody2D>();
 
                 switch (weaponSystem.weaponSelected)
                 {
                     case "Grenade":
-                        throwGrenade();
+                        StartCoroutine(throwGrenade());
                         break;
                     case "Boomerang":
-                        throwBoomerang();
+                        shootBoomerang();
+                        break;
+                    case "Plasma Orb":
+                        StartCoroutine(throwPlasmaOrb());
                         break;
                     default:
                         Debug.LogError("You haven't specified how to shoot this particular object");
@@ -55,14 +77,16 @@ public class Shooting : MonoBehaviour
             }
         }
 
-        //Weapons where you hold the right mouse button down to continously shoot
+        //Weapons where you hold the right mouse button down to continously shoot 
         if (Input.GetMouseButton(0) && weaponSystem.weaponSelected != null)
         {
             if (weaponSystem.getAmmo() > 0 && timeLeftBtwnShots <= 0 && weaponSystem.getWeapon().tag == "spamFire")
             {
                 weapon = weaponSystem.getWeapon();
-                weaponSystem.usedOneAmmo();
+                weaponSystem.useOneAmmo();
                 timeLeftBtwnShots = plasmaFireRate;
+                objectRig = weapon.transform.GetComponent<Rigidbody2D>();
+
 
                 switch (weaponSystem.weaponSelected)
                 {
@@ -81,69 +105,115 @@ public class Shooting : MonoBehaviour
     }
 
 
-    private Vector2 aimDirection()
+    private void aimWithGun()
     {
-        //calculate direction to throw object
-        Vector2 dir = (Input.mousePosition - camera.WorldToScreenPoint(shootingArm.position)).normalized;
+        //calculate direction to throw or shoot object in
+        aimDir = (Input.mousePosition - camera.WorldToScreenPoint(shootingArm.position)).normalized;
 
-        weapon.transform.position = bulletSpawnPoint.position;
-        weapon.layer = LayerMask.NameToLayer("Thrown Object");
+        weapon.transform.position = gunSpawnPoint.position;
+        weapon.layer = LayerMask.NameToLayer("Deployed Object");
         weapon.SetActive(true);
 
         weapon.transform.GetComponent<Collider2D>().isTrigger = false;
         weapon.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-
-        return dir;
     }
 
-    private void throwGrenade()
+    private IEnumerator aimWithHands()
     {
-        //get throw direction from mouse input
-        Vector2 dir = aimDirection();
-        Rigidbody2D objectRig = weapon.transform.GetComponent<Rigidbody2D>();
+        //calculate direction to throw or shoot object in
+        aimDir = (Input.mousePosition - camera.WorldToScreenPoint(shootingArm.position)).normalized;
+        armAnimator.SetInteger("Arms Phase", 1);
+
+        yield return new WaitForSeconds(0.3f);
+
+        weapon.transform.position = handSpawnPoint.position;
+        weapon.layer = LayerMask.NameToLayer("Deployed Object");
+        weapon.SetActive(true);
+
+        weapon.transform.GetComponent<Collider2D>().isTrigger = false;
+        weapon.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
+
+    private IEnumerator throwGrenade()
+    {
+        //get aim direction from mouse input
+        StartCoroutine(aimWithHands());
+        yield return new WaitForSeconds(0.3f);
+
+        //apply a large force to throw the grenadeaa
+        Vector2 unadjustedForce = grenadeThrowForce * aimDir * new Vector2(1.2f, 1) + new Vector2(0, grenadeYForce);
+        objectRig.velocity = new Vector2(0, 0);
+        objectRig.AddForce(unadjustedForce * objectRig.mass);
+    }
+
+
+    private IEnumerator throwPlasmaOrb()
+    {
+        //get aim direction from mouse input
+        StartCoroutine(aimWithHands());
+        yield return new WaitForSeconds(0.3f);
 
         //apply a large force to throw the grenade
-        Vector2 unadjustedForce = grenadeThrowForce * dir * new Vector2(1.2f, 1) + new Vector2(0, grenadeYForce);
+        Vector2 unadjustedForce = grenadeThrowForce * aimDir * new Vector2(1.2f, 1) + new Vector2(0, grenadeYForce);
         objectRig.velocity = new Vector2(0, 0);
         objectRig.AddForce(unadjustedForce * objectRig.mass);
     }
 
     private void shootPlasmaBullet()
     {
-        //get throw direction from mouse input
-        Vector2 dir = aimDirection();
-        Rigidbody2D objectRig = weapon.transform.GetComponent<Rigidbody2D>();
+        //get aim direction from mouse input
+        aimWithGun();
 
         //spawn and orient the bullet correctly
-        weapon.transform.right = dir;
-        objectRig.velocity = dir * plasmaBulletSpeed;
+        weapon.transform.right = aimDir;
+        objectRig.velocity = aimDir * plasmaBulletSpeed;
     }
 
-    private void throwBoomerang()
+    private void shootBoomerang()
     {
-        //get throw direction from mouse input
-        Vector2 throwDir = aimDirection();
-        Rigidbody2D objectRig = weapon.transform.GetComponent<Rigidbody2D>();
+        //get aim direction from mouse input
+        aimWithGun();
 
         //set the boomerang's velocity really high
-        objectRig.velocity = throwDir * boomerangSpeed;
+        objectRig.velocity = aimDir * boomerangSpeed;
         objectRig.angularVelocity = Random.Range(objectSpinSpeed.x, objectSpinSpeed.y);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void switchMode(string type)
     {
-        //Player collides with weapon, so equip it
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Object"))
+        if (type == "gun")
         {
-            weaponSystem.EquipNewWeapon(collision.gameObject.tag);
-            collision.gameObject.SetActive(false);
+            shoulderBones_GUN.SetActive(true);
+            shoulderSprites_GUN.SetActive(true);
+            frontShoulderBone_HAND.gameObject.SetActive(false);
+            backShoulderBone_HAND.gameObject.SetActive(false);
+        }
+
+        else if (type == "hands")
+        {
+            shoulderBones_GUN.SetActive(false);
+            shoulderSprites_GUN.SetActive(false);
+            frontShoulderBone_HAND.gameObject.SetActive(true);
+            backShoulderBone_HAND.gameObject.SetActive(true);
+
+            armAnimator.SetInteger("Arms Phase", 0);
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //Player collides with weapon, so equip it
+        if (col.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            weaponSystem.EquipNewWeapon(col.gameObject.tag, 25);
+            col.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
     {
         //Player is on a levitation boost platform and clicks W -> give them a jump boost 
-        if (collision.gameObject.tag == "Levitation" && Input.GetKeyDown(KeyCode.W) && sideview_Controller.grounded)
+        if (col.gameObject.tag == "Levitation" && Input.GetKeyDown(KeyCode.W) && sideview_Controller.grounded)
             rig.AddForce(Constants.levitationBoost);
     }
 }
--- Panda Fighter/Assets/Scripts/Player/WeaponSystem.cs
+++ Panda Fighter/Assets/Scripts/Player/WeaponSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,6 @@ public class WeaponSystem : MonoBehaviour
     private Dictionary<string, Sprite> notEquipped = new Dictionary<string, Sprite>();
 
     private Dictionary<string, List<Transform>> physicalWeapons = new Dictionary<string, List<Transform>>();
-
     public Transform inventory;
     public Transform weaponSprites;
     public Transform physicalWeapon;
@@ -22,8 +22,13 @@ public class WeaponSystem : MonoBehaviour
     public string weaponSelected;
     private int bulletNumber = 0;
 
+    private Shooting shooting;
+
     void Awake()
     {
+        //define components
+        shooting = transform.GetComponent<Shooting>();
+
         //add each weapon's image + ammo text to a dictionary, accessible by weapon tag
         foreach (Transform weapon in inventory)
         {
@@ -48,16 +53,35 @@ public class WeaponSystem : MonoBehaviour
                 physicalAmmo.Add(ammo);
             physicalWeapons.Add(weaponType.tag, physicalAmmo); ;
         }
-
-        //start with grenade equipped
-        weaponSelected = "Pistol";
-        EquipNewWeapon("Pistol");
     }
 
-    //allow player to select a different weapon 
-    public void SelectWeapon()
+    // --------------------------------------------------------------------
+    // Default weapon you start with
+    // --------------------------------------------------------------------
+    void Start()
     {
-        string weapon = EventSystem.current.currentSelectedGameObject.transform.tag;
+        EquipNewWeapon("Pistol", 25);
+        SelectWeapon("Pistol", "gun");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("0"))
+            SelectWeapon("Grenade", "hands");
+        if (Input.GetKeyDown("1"))
+            SelectWeapon("Boomerang", "gun");
+        if (Input.GetKeyDown("2"))
+            SelectWeapon("Pistol", "gun");
+        if (Input.GetKeyDown("3"))
+            SelectWeapon("Plasma Orb", "hands");
+    }
+
+    // --------------------------------------------------------------------
+    // FOR MOBILE VERSION: allow player to tap to select a different weapon 
+    // --------------------------------------------------------------------
+    public void SelectWeapon(string useMode)
+    {
+        string weapon = (EventSystem.current.currentSelectedGameObject) ? EventSystem.current.currentSelectedGameObject.transform.tag : "Pistol";
         int weaponAmmo = Int32.Parse(ammo[weapon].text);
 
         //if that weapon has ammo, equip it
@@ -68,18 +92,41 @@ public class WeaponSystem : MonoBehaviour
 
         //update which bullet in the bullet list to use
         bulletNumber = ++bulletNumber % physicalWeapons[weaponSelected].Count;
+        shooting.switchMode(useMode);
     }
 
-    //player collects a weapon by physically touching it
-    public void EquipNewWeapon(string weapon)
+    // --------------------------------------------------------------------
+    // FOR PC VERSION: allow player to select a different weapon 
+    // --------------------------------------------------------------------
+    public void SelectWeapon(string weapon, string useMode)
+    {
+        int weaponAmmo = Int32.Parse(ammo[weapon].text);
+
+        //if that weapon has ammo, equip it
+        if (weaponAmmo > 0)
+            weaponSelected = weapon;
+        else
+            return;
+
+        //update which bullet in the bullet list to use
+        bulletNumber = ++bulletNumber % physicalWeapons[weaponSelected].Count;
+        shooting.switchMode(useMode);
+    }
+
+    // --------------------------------------------------------------------
+    // Player collects a weapon by physically touching it
+    // --------------------------------------------------------------------
+    public void EquipNewWeapon(string weapon, int bullets)
     {
         //update weapon sprite + ammo
         weapons[weapon].sprite = equipped[weapon];
-        ammo[weapon].text = "1000";
+        ammo[weapon].text = bullets.ToString();
     }
 
-    //player uses up ammo of a certain weapon
-    public void usedOneAmmo()
+    // --------------------------------------------------------------------
+    // Player uses up ammo of a certain weapon
+    // --------------------------------------------------------------------
+    public void useOneAmmo()
     {
         //decrease ammo by 1 and update ammo text
         int weaponAmmo = Int32.Parse(ammo[weaponSelected].text);
@@ -94,14 +141,18 @@ public class WeaponSystem : MonoBehaviour
             weapons[weaponSelected].sprite = notEquipped[weaponSelected];
     }
 
-    //get the weapon bullet from the list in the dictionary + return different bullet next time
+    // --------------------------------------------------------------------
+    // Get the weapon bullet from the list in the dictionary + return different bullet next time
+    // --------------------------------------------------------------------
     public GameObject getWeapon()
     {
         Transform theWeapon = physicalWeapons[weaponSelected][bulletNumber];
         return theWeapon.gameObject;
     }
 
-    //get the weapon's ammo
+    // --------------------------------------------------------------------
+    // Get the weapon's ammo
+    // --------------------------------------------------------------------
     public int getAmmo()
     {
         return Int32.Parse(ammo[weaponSelected].text);
