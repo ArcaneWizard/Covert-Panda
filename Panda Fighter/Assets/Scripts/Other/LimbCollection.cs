5f12c95e4b6ca4d9ffa96d54091e6eaaa9cfdadc
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-12-21 02:54:38 -0800

    added enemy heads

---
+++ Panda Fighter/Assets/Scripts/Other/LimbCollection.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class LimbCollection : MonoBehaviour
+{
+    public Sprite chest;
+    public Sprite mainArmPad;
+    public Sprite mainArm;
+    public Sprite mainHand;
+    public Sprite backArmPad;
+    public Sprite backArm;
+    public Sprite backHand;
+    public Sprite leftThigh;
+    public Sprite leftLeg;
+    public Sprite leftFoot;
+    public Sprite rightThigh;
+    public Sprite rightLeg;
+    public Sprite rightFoot;
+    public Sprite head;
+
+    public Dictionary<limbTypes, Sprite> sprites = new Dictionary<limbTypes, Sprite>();
+
+#if (UNITY_EDITOR)
+    void OnValidate()
+    {
+        if (!EditorApplication.isPlayingOrWillChangePlaymode)
+            updateSprites();
+    }
+#endif
+
+    private void updateSprites()
+    {
+        sprites.Clear();
+
+        sprites.Add(limbTypes.chest, chest);
+        sprites.Add(limbTypes.leftThigh, leftThigh);
+        sprites.Add(limbTypes.rightThigh, rightThigh);
+        sprites.Add(limbTypes.leftFoot, leftFoot);
+        sprites.Add(limbTypes.rightFoot, rightFoot);
+        sprites.Add(limbTypes.leftLeg, leftLeg);
+        sprites.Add(limbTypes.rightLeg, rightLeg);
+        sprites.Add(limbTypes.mainArm, mainArm);
+        sprites.Add(limbTypes.backArm, backArm);
+        sprites.Add(limbTypes.mainArmPad, mainArmPad);
+        sprites.Add(limbTypes.backArmPad, backArmPad);
+        sprites.Add(limbTypes.mainHand, mainHand);
+        sprites.Add(limbTypes.backHand, backHand);
+        sprites.Add(limbTypes.head, head);
+    }
+
+    public Sprite returnSprite(limbTypes limbType)
+    {
+        if (sprites.Count == 0)
+            updateSprites();
+
+        return sprites[limbType];
+    }
+}
+
+public enum limbTypes
+{
+    chest,
+    mainArmPad,
+    mainArm,
+    mainHand,
+    backArmPad,
+    backArm,
+    backHand,
+    leftThigh,
+    leftLeg,
+    leftFoot,
+    rightThigh,
+    rightLeg,
+    rightFoot,
+    head
+}
---
+++ Panda Fighter/Assets/Scripts/Other/LimbSwapper.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class LimbSwapper : MonoBehaviour
+{
+    public limbTypes limbType;
+
+    private Transform armBones;
+    private LimbCollection spriteCollection;
+
+    private SpriteRenderer sR;
+    private UnityEngine.U2D.Animation.SpriteSkin spriteSkin;
+
+#if (UNITY_EDITOR)
+    void OnValidate()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+            return;
+
+        initializeVariables();
+        updateSpriteAndBoneTransforms();
+    }
+
+    void Update()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+            return;
+
+        initializeVariables();
+        if (sR.sprite != spriteCollection.returnSprite(limbType))
+            updateSpriteAndBoneTransforms();
+    }
+#endif
+
+    // Initialize the entity's collection of sprites for each limb. Using that, 
+    // update the sprite for this gameobject based on the limb type selected. Also
+    // update the bone associated with this sprite to be the same one associated with
+    // the previous sprite.
+    private void updateSpriteAndBoneTransforms()
+    {
+        sR.sprite = spriteCollection.returnSprite(limbType);
+        if (spriteSkin.boneTransforms.Length > 0)
+            spriteSkin.boneTransforms[0] = spriteSkin.rootBone;
+    }
+
+    // Initialize the entity's collection of sprites for each limb 
+    private void findSpriteCollection()
+    {
+        if (!armBones)
+            armBones = transform.parent;
+
+        while (!spriteCollection && armBones)
+        {
+            if (armBones.GetComponent<LimbCollection>())
+                spriteCollection = armBones.GetComponent<LimbCollection>();
+            else
+                armBones = armBones.parent;
+        }
+    }
+
+    // Initialize the private fields if they aren't already defined
+    private void initializeVariables()
+    {
+        if (!sR)
+            sR = transform.GetComponent<SpriteRenderer>();
+
+        if (!spriteCollection)
+            findSpriteCollection();
+
+        if (!spriteSkin)
+            spriteSkin = transform.GetComponent<UnityEngine.U2D.Animation.SpriteSkin>();
+    }
+}
+
