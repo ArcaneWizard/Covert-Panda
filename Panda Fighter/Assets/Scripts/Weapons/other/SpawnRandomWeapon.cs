117966e4500344895356d5fd42b3b2028ebbd73f
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2022-04-16 19:17:31 -0700

    reorganized folders

--- Panda Fighter/Assets/Scripts/Weapons/other/SpawnRandomWeapon.cs
+++ Panda Fighter/Assets/Scripts/Weapons/other/SpawnRandomWeapon.cs
@@ -8,13 +8,13 @@ public class SpawnRandomWeapon : MonoBehaviour
     private float timer = 0f;
     private Vector2 timeTillNextSpawn = new Vector2(6, 9);
 
-    private PickableWeapons pickableWeapons;
+    private SpawnWeaponManager spawnWeaponManager;
     private bool canSpawnWeapon;
     
     void Start() 
     {
         transform.localPosition = new Vector3(0,0,0);
-        pickableWeapons = transform.parent.parent.GetComponent<PickableWeapons>();
+        spawnWeaponManager = transform.parent.parent.GetComponent<SpawnWeaponManager>();
         spawnRandomWeapon();
     }
 
@@ -29,9 +29,9 @@ public class SpawnRandomWeapon : MonoBehaviour
 
     private void spawnRandomWeapon() 
     {
-         int random = UnityEngine.Random.Range(0, pickableWeapons.Weapons.Count);
-         string weaponTag = pickableWeapons.Weapons.Keys.ToArray()[random];
-         transform.GetChild(pickableWeapons.Weapons[weaponTag]).gameObject.SetActive(true);
+         int random = UnityEngine.Random.Range(0, spawnWeaponManager.AvailableWeapons.Count);
+         string weaponTag = spawnWeaponManager.AvailableWeapons.Keys.ToArray()[random];
+         transform.GetChild(spawnWeaponManager.AvailableWeapons[weaponTag]).gameObject.SetActive(true);
 
          canSpawnWeapon = false;
     }
---
+++ Panda Fighter/Assets/Scripts/Weapons/other/SpawnWeaponManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWeaponManager : MonoBehaviour
+{
+    // collection of all weapons that can be spawned in this level
+    public Dictionary<string, int> AvailableWeapons {get; private set;}
+
+    // Initializes the weapons that can be spawned in this level
+    // FUTURE TO-DO: make it read off a text file (saves which list of weapons the user has chosen for a level)
+    private void Awake() 
+    {
+        AvailableWeapons = new Dictionary<string, int>();
+
+        if (!transform.GetChild(0))
+            Debug.LogError("Need at least one weapon holder");
+
+        Transform spawner = transform.GetChild(0).GetChild(1);
+        for (int index = 0; index < spawner.childCount; index++)
+            AvailableWeapons.Add(spawner.GetChild(index).GetComponent<WeaponTag>().Tag, index);
+    }
+}
