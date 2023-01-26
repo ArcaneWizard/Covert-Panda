0fd4514b4f30d439d33a5f93e82debf702ae4da8
Raghav Narula <raghavnarula.77@gmail.com>
2023-01-26 07:10:37 -0800

    refactoring TrajectoryPaths 1/2

--- Panda Fighter/Assets/Scripts/AI/Trajectories/TrajectoryPath.cs
+++ Panda Fighter/Assets/Scripts/AI/Trajectories/TrajectoryPath.cs
@@ -27,7 +27,7 @@ public class TrajectoryPath : MonoBehaviour
     [Header("Other Settings")]
     public Vector2 jumpBounds = new Vector2(-1f, -1f);
     public int considerationWeight = 1;
-    public float lengthShown = 14;
+    public float timeShown = 2.8f;
 
     [Header("Connected Zone")]
     public int chainedDecisionZone = -1;
@@ -35,6 +35,8 @@ public class TrajectoryPath : MonoBehaviour
     private float yVelocity;
     private float x;
     private float y;
+
+    private const float epsilon = 0.001f;
     //private Vector2 lastPointPlotted;
     //private Vector2 lastP_b4DirSwitch;
 
@@ -64,7 +66,7 @@ public class TrajectoryPath : MonoBehaviour
         else if (ActionType == AIActionType.LaunchPad)
         {
             transform.name = "Launch Pad";
-            drawJumpPadArc(defaultGravity, lengthShown, launchPadForce);
+            drawJumpPadArc(defaultGravity, timeShown, launchPadForce);
             showGizmoJumpBounds();
         }
 
@@ -79,8 +81,8 @@ public class TrajectoryPath : MonoBehaviour
         else if (ActionType == AIActionType.NormalJump)
         {
             transform.name = "Normal Jump";
-            drawNormalJump(defaultGravity, lengthShown, jumpForce, speedRange.x);
-            drawNormalJump(defaultGravity, lengthShown, jumpForce, speedRange.y);
+            drawNormalJump(defaultGravity, jumpForce, speedRange.x);
+            drawNormalJump(defaultGravity, jumpForce, speedRange.y);
             showGizmoJumpBounds();
         }
 
@@ -153,18 +155,17 @@ public class TrajectoryPath : MonoBehaviour
     // draws a straight line journey on the scene view
     private void drawStraightLine()
     {
-        this.gravity = 0;
-        
-        Vector2 pointRightB4VelocityChange = plotJourney(4, transform.position, speedRange.x, 0);
+        gravity = 0;
+        Vector2 pointRightB4VelocityChange = drawTrajectory(0.8f, 5f, transform.position, speedRange.x, 0);
     }
 
     // draws a normal jump journey on the scene view
-    private void drawNormalJump(float gravity, float lengthShown, float jumpForce, float speed)
+    private void drawNormalJump(float gravity, float jumpForce, float speed)
     {
         this.gravity = gravity;
 
-        Vector2 pointRightB4VelocityChange = plotJourneyVeryAccurately(timeB4Change.x * 5f, transform.position, speed, jumpForce);
-        plotJourneyAfterChangingSpeedOnce(lengthShown, pointRightB4VelocityChange, transform.position, speed, timeB4Change.x, changedSpeed.x, jumpForce);
+        Vector2 pointRightB4VelocityChange = drawTrajectory(timeB4Change.x, 20f, transform.position, speed, jumpForce);
+        plotJourneyAfterChangingSpeedOnce(timeShown, pointRightB4VelocityChange, transform.position, speed, timeB4Change.x, changedSpeed.x, jumpForce);
     }
 
     // draws the double jump journey on the scene view. first draws the normal jump up till right b4 
@@ -173,9 +174,9 @@ public class TrajectoryPath : MonoBehaviour
     {
         this.gravity = defaultGravity;
 
-        Vector2 pointRightB4DoubleJumping = plotJourneyVeryAccurately(timeB4DoubleJump * 5f, transform.position, speedRange.x, jumpForce);
-        Vector2 pointRightB4VelocityChange = plotJourneyVeryAccurately(timeB4SecondChange.x * 5f, pointRightB4DoubleJumping, changedSpeed.x, doubleJumpForce);
-        plotJourneyAfterChangingSpeedOnce(lengthShown, pointRightB4VelocityChange, pointRightB4DoubleJumping, changedSpeed.x, timeB4SecondChange.x, secondChangedSpeed.x, doubleJumpForce);
+        Vector2 pointRightB4DoubleJumping = drawTrajectory(timeB4DoubleJump, 20f, transform.position, speedRange.x, jumpForce);
+        Vector2 pointRightB4VelocityChange = drawTrajectory(timeB4SecondChange.x, 20f, pointRightB4DoubleJumping, changedSpeed.x, doubleJumpForce);
+        plotJourneyAfterChangingSpeedOnce(timeShown, pointRightB4VelocityChange, pointRightB4DoubleJumping, changedSpeed.x, timeB4SecondChange.x, secondChangedSpeed.x, doubleJumpForce);
     }
     
     // draws the fall down arc journey on the scene view. first draws the fall down path till right b4
@@ -184,8 +185,8 @@ public class TrajectoryPath : MonoBehaviour
     {
         this.gravity = defaultGravity;
 
-        Vector2 pointRightB4ChangingSpeedHorizontally = plotJourneyVeryAccurately(timeB4DirSwitch * 5f + 0.01f, transform.position, speedRange.x, 0);
-        plotJourneyWithTimeOffset(lengthShown, pointRightB4ChangingSpeedHorizontally, changedSpeed.x, 0, timeB4DirSwitch * 5f);
+        Vector2 pointRightB4ChangingSpeedHorizontally = drawTrajectory(timeB4DirSwitch, 20f, transform.position, speedRange.x, 0);
+        plotJourneyWithTimeOffset(timeShown, pointRightB4ChangingSpeedHorizontally, changedSpeed.x, 0, timeB4DirSwitch * 5f);
     }
 
     // draws the jump pad boost journey on the scene view. first draws the upwards path till right b4 the
@@ -194,7 +195,7 @@ public class TrajectoryPath : MonoBehaviour
     {
         this.gravity = gravity;
 
-        Vector2 pointB4ChangingSpeedOnce = plotJourneyVeryAccurately(timeB4Change.x * 5, transform.position, speedRange.x, jumpForce);
+        Vector2 pointB4ChangingSpeedOnce = drawTrajectory(timeB4Change.x, 20f, transform.position, speedRange.x, jumpForce);
         Vector2 pointB4ChangingSpeedTwice = plotJourneyAfterChangingSpeedOnce(timeB4SecondChange.x * 5, pointB4ChangingSpeedOnce, transform.position, speedRange.x,
             timeB4Change.x, changedSpeed.x, jumpForce);
         plotJourneyAfterChangingSpeedTwice(lengthShown, pointB4ChangingSpeedTwice, transform.position, speedRange.x, timeB4Change.x, changedSpeed.x, timeB4SecondChange.x,
@@ -207,7 +208,7 @@ public class TrajectoryPath : MonoBehaviour
     
     // returns the position the AI assuming the AI had the given horizontal speed and jump force at
      // the given start position, and traveled at that speed for some time 
-    private Vector2 pointAlongShortJourney(float time, Vector2 start, float speed, float jumpForce)
+    private Vector2 calculatePosition(float time, Vector2 start, float speed, float jumpForce)
     {
         x = start.x + speed * time * movementDirX;
 
@@ -221,7 +222,7 @@ public class TrajectoryPath : MonoBehaviour
     // returns the position the AI will be assuming the AI had the given horizontal speed and jump force at
     // the given start position, traveled at that speed for some time (speed / time), changed it's speed midair and
     // continued at that for a few seconds (speed 2 / time 2)
-    private Vector2 pointAlongMediumJourney(float time, Vector2 start, float speed, float time2, float speed2, float jumpForce)
+    private Vector2 calculatePosition(float time, Vector2 start, float speed, float time2, float speed2, float jumpForce)
     {
         x = start.x + movementDirX * (speed * time + speed2 * time2);
 
@@ -236,7 +237,7 @@ public class TrajectoryPath : MonoBehaviour
      // the given start position,  traveled at that speed for some time (speed / time), changed it's speed midair and continued
      // at that for an additional few seconds (speed 2 / time 2), and later changed it's speed for a second time + continued at that 
      // for a few seconds (speed 3 / time 3)
-    private Vector2 pointAlongLongJourney(float time, Vector2 start, float speed, float time2, float speed2, float time3, float speed3, float jumpForce)
+    private Vector2 calculatePosition(float time, Vector2 start, float speed, float time2, float speed2, float time3, float speed3, float jumpForce)
     {
         x = start.x + movementDirX * (speed * time + speed2 * time2 + speed3 * time3);
 
@@ -248,113 +249,101 @@ public class TrajectoryPath : MonoBehaviour
     }
 
     //----------------------------------------------------------------------------------------------------------------
-    //----------------------------- HELPER METHOS TO PLOT THE ARC ON THE SCENE VIEW  ---------------------------------
+    //----------------------------- HELPER METHODS TO DRAW THE TRAJECTORY ON THE SCENE VIEW  ---------------------------------
     //----------------------------------------------------------------------------------------------------------------
 
-    // draws journey on scene view using medium length line segments. Takes in a start position, initial velocity, initial jump force, and how
-    // many segments to draw b4 stopping. The segements will be blue/green if the journey goes to the right, and red/magenta if it goes to the left
-    private Vector2 plotJourney(float linesDrawn, Vector2 start, float speed, float jumpForce)
+    // Draws trajectory on scene view quite accurately by using line segments. The trajectory requires an initial
+    // position, initial X velocity, and initial vertical force. This function also requires the duration of the trajectory
+    // drawn (seconds), and how many line segments to draw per second of trajectory time (more line segments used = better curves drawn).
+    private Vector2 drawTrajectory(float seconds, float linesPerSecond, Vector2 initPos, float initXVelocity, float initYForce)
     {
-        Vector2 lastPointPlotted = start;
-        for (float i = 0; i <= linesDrawn; i++)
+        float increment = 1f / linesPerSecond;
+
+        Vector2 prevPointPosition = initPos;
+        for (float i = 0; i <= seconds; i += increment)
         {
             if (movementDirX == 1)
-                Gizmos.color = i % 2 == 0 ? Color.blue : Color.green;
+                Gizmos.color = i % (2*increment) < epsilon ? Color.blue : Color.green;
             else
-                Gizmos.color = i % 2 == 0 ? Color.red : Color.magenta;
+                Gizmos.color = i % (2*increment) < epsilon ? Color.red : Color.magenta;
 
-            Vector2 nextPointPlotted = pointAlongShortJourney(i / 5f, start, speed, jumpForce);
-            Gizmos.DrawLine(lastPointPlotted, nextPointPlotted);
-            lastPointPlotted = nextPointPlotted;
+            Vector2 currPointPosition = calculatePosition(i, initPos, initXVelocity, initYForce);
+            Gizmos.DrawLine(prevPointPosition, currPointPosition);
+            prevPointPosition = currPointPosition;
         }
 
-        return lastPointPlotted;
-    }
-
-    // draws journey on scene view quite accurately by using tiny line segments. Takes in a start position, initial velocity, initial jump 
-    // force, and how many segments to draw b4 stopping. The segements will be blue/green if the journey goes to the right, and red/magenta 
-    // if it goes to the left
-    private Vector2 plotJourneyVeryAccurately(float linesDrawn, Vector2 start, float speed, float jumpForce)
-    {
-        Vector2 lastPointPlotted = start;
-        for (float i = 0; i <= linesDrawn; i += 0.25f)
-        {
-            if (movementDirX == 1)
-                Gizmos.color = i % 0.5f == 0 ? Color.blue : Color.green;
-            else
-                Gizmos.color = i % 0.5f == 0 ? Color.red : Color.magenta;
-
-            Vector2 nextPointPlotted = pointAlongShortJourney(i / 5f, start, speed, jumpForce);
-            Gizmos.DrawLine(lastPointPlotted, nextPointPlotted);
-            lastPointPlotted = nextPointPlotted;
-        }
-
-        return lastPointPlotted;
+        return prevPointPosition;
     }
 
     // draws journey on scene view, given a start position, initial velocity, initial jump force, and
     // how many segments to draw b4 stopping. Takes into account the time already fallen to figure out
     // the AI's vertical velocity at the start. Segments will be blue/green if the journey heads right 
     // or red/magenta if the journey heads Returns the point it stopped plotting at
-    private Vector2 plotJourneyWithTimeOffset(float linesDrawn, Vector2 start, float speed, float jumpForce, float timeAlreadyFallen)
+    private Vector2 plotJourneyWithTimeOffset(float seconds, Vector2 start, float speed, float jumpForce, float timeAlreadyFallen)
     {
-        Vector2 lastPointPlotted = start;
-        for (float i = 0; i <= linesDrawn; i += 0.25f)
+        float increment = 0.05f;
+
+        Vector2 prevPointPosition = start;
+        for (float i = 0; i <= seconds; i += increment)
         {
             if (movementDirX == 1)
-                Gizmos.color = i % 2 == 0 ? Color.blue : Color.green;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.blue : Color.green;
             else
-                Gizmos.color = i % 2 == 0 ? Color.red : Color.magenta;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.red : Color.magenta;
 
-            Vector2 nextPointPlotted = pointAlongShortJourney(timeAlreadyFallen + i / 5, start, speed, jumpForce);
-            Gizmos.DrawLine(lastPointPlotted, nextPointPlotted);
-            lastPointPlotted = nextPointPlotted;
+            Vector2 currPointPosition = calculatePosition(timeAlreadyFallen + i, start, speed, jumpForce);
+            Gizmos.DrawLine(prevPointPosition, currPointPosition);
+            prevPointPosition = currPointPosition;
         }
 
-        return lastPointPlotted;
+        return prevPointPosition;
     }
 
     // draws journey on scene view using medium length line segments. Takes in a how many segments to draw b4 stopping,
     // the point to continue plotting from and parameters of the pointAlongMediumJourney() method. Segements will be blue/green 
     // if the journey heads right or red/magenta if the journey heads left. Returns the point it stopped plotting at
-    private Vector2 plotJourneyAfterChangingSpeedOnce(float linesDrawn, Vector2 lastPointPosition, Vector2 start, float speed, float time, float speed2,
+    private Vector2 plotJourneyAfterChangingSpeedOnce(float seconds, Vector2 lastPointPosition, Vector2 start, float speed, float time, float speed2,
         float jumpForce)
     {
-        Vector2 lastPointPlotted = lastPointPosition;
-        for (float i = 0; i <= linesDrawn; i++)
+        float increment = 0.05f;
+
+        Vector2 prevPointPosition = lastPointPosition;
+        for (float i = 0; i <= seconds; i += increment)
         {
             if (movementDirX == 1)
-                Gizmos.color = i % 2 == 0 ? Color.blue : Color.green;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.blue : Color.green;
             else
-                Gizmos.color = i % 2 == 0 ? Color.red : Color.magenta;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.red : Color.magenta;
 
-            Vector2 newPointPlotted = pointAlongMediumJourney(time, start, speed, i/5f, speed2, jumpForce);
-            Gizmos.DrawLine(lastPointPlotted, newPointPlotted);
-            lastPointPlotted = newPointPlotted;
+            Vector2 currPointPosition = calculatePosition(time, start, speed, i, speed2, jumpForce);
+            Gizmos.DrawLine(prevPointPosition, currPointPosition);
+            prevPointPosition = currPointPosition;
         }
 
-        return lastPointPlotted;
+        return prevPointPosition;
     }
 
     // draws journey on scene view using medium length line segments. Takes in a how many segments to draw b4 stopping,
     // the point to continue plotting from and parameters of the pointAlongLongJourney() method. Segements will be blue/green 
     // if the journey heads right or red/magenta if the journey heads left. Returns the point it stopped plotting at
-    private Vector2 plotJourneyAfterChangingSpeedTwice(float linesDrawn, Vector2 lastPointPosition, Vector2 start, float speed, float time, float speed2, 
+    private Vector2 plotJourneyAfterChangingSpeedTwice(float seconds, Vector2 lastPointPosition, Vector2 start, float speed, float time, float speed2, 
         float time2, float speed3, float jumpForce)
     {
-        Vector2 lastPointPlotted = lastPointPosition;
-        for (float i = 0; i <= linesDrawn; i++)
+        float increment = 0.05f;
+
+        Vector2 prevPointPosition = lastPointPosition;
+        for (float i = 0; i <= seconds; i += increment)
         {
             if (movementDirX == 1)
-                Gizmos.color = i % 2 == 0 ? Color.blue : Color.green;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.blue : Color.green;
             else
-                Gizmos.color = i % 2 == 0 ? Color.red : Color.magenta;
+                Gizmos.color = i % (2 * increment) < epsilon ? Color.red : Color.magenta;
 
-            Vector2 newPointPlotted = pointAlongLongJourney(time, start, speed, time2, speed2, i/5f, speed3, jumpForce);
-            Gizmos.DrawLine(lastPointPlotted, newPointPlotted);
-            lastPointPlotted = newPointPlotted;
+            Vector2 currPointPosition = calculatePosition(time, start, speed, time2, speed2, i, speed3, jumpForce);
+            Gizmos.DrawLine(prevPointPosition, currPointPosition);
+            prevPointPosition = currPointPosition;
         }
 
-        return lastPointPlotted;
+        return prevPointPosition;
     }
 }
